8c0f44f3a8671628d0044db8ba0c9e4f9c5fe8f0
KI YANG <yang60584@gotoltc.edu>
2018-04-12 17:20:28 -0500

    Implemented SQL, Saves entries into server/tables

--- TicketDominator/TicketDominator.WebUI/App_Start/UnityConfig.cs
+++ TicketDominator/TicketDominator.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using System;
 using TicketDominator.Core.Contracts;
 using TicketDominator.Core.Models;
 using TicketDominator.DataAccess.InMemory;
+using TicketDominator.DataAccess.SQL;
 using Unity;
 
 namespace TicketDominator.WebUI
@@ -44,8 +45,8 @@ namespace TicketDominator.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
